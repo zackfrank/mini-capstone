0c91109f7168f6d5366e112498b4df414b97d28a
Zack Frank <frank.zack@gmail.com>
2018-03-06 19:43:40 -0600

    Add all_products route and frontend.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,17 @@
 class ProductsController < ApplicationController
+  def all_products
+    products = Product.all
+
+    products_array = products.map { |shirt| 
+      {
+      name: shirt.name,
+      size: shirt.size,
+      price: shirt.price,
+      image_url: shirt.image_url,
+      description: shirt.description
+      }
+     }
+
+    render json: products_array.as_json
+  end
 end
---
+++ frontend.rb
@@ -0,0 +1,20 @@
+require "unirest"
+
+system "clear"
+
+while true do 
+  puts "Welcome to the T-Shirt Shop!"
+  puts "Please choose an option below:"
+  puts "To view all products, type [1]"
+  puts "To quit, type 'q'"
+  input = gets.chomp
+
+  if input == "1"
+    response = Unirest.get("http://localhost:3000/all_products")
+    page = response.body
+
+    puts JSON.pretty_generate(page)
+  elsif input == 'q'
+    break
+  end
+end
