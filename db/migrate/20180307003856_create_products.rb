0166fd6f822a0b9e42aca2923ad6c0dd41abc9ec
Zack Frank <frank.zack@gmail.com>
2018-03-06 19:07:00 -0600

    Add products to seeds file

---
+++ db/migrate/20180307003856_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :size
+      t.integer :price
+      t.string :image_url
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: "Shirt 1 - XS", size: "XS", price: 15, description: "Super soft, organic cotton, short-sleeve t-shirt")
+Product.create(name: "Shirt 1 - SM", size: "Small", price: 15, description: "Super soft, organic cotton, short-sleeve t-shirt")
+Product.create(name: "Shirt 1 - MD", size: "Medium", price: 15, description: "Super soft, organic cotton, short-sleeve t-shirt")
+Product.create(name: "Shirt 1 - LG", size: "Large", price: 15, description: "Super soft, organic cotton, short-sleeve t-shirt")
+Product.create(name: "Shirt 1 - XL", size: "XL", price: 15, description: "Super soft, organic cotton, short-sleeve t-shirt")
+Product.create(name: "Shirt 2 - SM", size: "Small", price: 15, description: "Super soft, organic cotton, short-sleeve, v-neck t-shirt")
+Product.create(name: "Shirt 2 - MD", size: "Medium", price: 15, description: "Super soft, organic cotton, short-sleeve, v-neck t-shirt")
+Product.create(name: "Shirt 2 - LG", size: "Large", price: 15, description: "Super soft, organic cotton, short-sleeve, v-neck t-shirt")
